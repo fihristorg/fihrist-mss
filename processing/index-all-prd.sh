66ae11c0b3c17e6ffcf3efec202afaebc756e606
Unknown <andrew.morrison@bodleian.ox.ac.uk>
2018-02-23 17:39:55 +0000

    New: Indexing control scripts

--- processing/index-all-prd.sh
+++ processing/index-all-prd.sh
@@ -15,13 +15,13 @@ cd "${0%/*}"
 echo "Warning: This script takes a couple of hours to run, due to the size of the Fihrist collection."
 
 # Re-index manuscripts (includes rebuilding customized manuscript HTML pages, which must be run first)
-./generate-html.sh && ./generate-solr-document.sh manuscripts.xquery mss_index.xml manuscript solr01-qa.bodleian.ox.ac.uk $1
+./generate-html.sh && ./generate-solr-document.sh manuscripts.xquery mss_index.xml manuscript solr01-prd.bodleian.ox.ac.uk $1
 
 # Reindex works
-./generate-solr-document.sh works.xquery works_index.xml work solr01-qa.bodleian.ox.ac.uk $1
+./generate-solr-document.sh works.xquery works_index.xml work solr01-prd.bodleian.ox.ac.uk $1
 
 # Reindex people
-./generate-solr-document.sh people.xquery people_index.xml person solr01-qa.bodleian.ox.ac.uk $1
+./generate-solr-document.sh people.xquery people_index.xml person solr01-prd.bodleian.ox.ac.uk $1
 
 # Reindex subjects (including places)
-./generate-solr-document.sh subjects.xquery subjects_index.xml subject solr01-qa.bodleian.ox.ac.uk $1
+./generate-solr-document.sh subjects.xquery subjects_index.xml subject solr01-prd.bodleian.ox.ac.uk $1
